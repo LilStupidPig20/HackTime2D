4c31ff270d2a8956caf9bf6204ee469426fefadc
LilStupidPig20 <mistersaxar@gmail.com>
2020-12-24 23:12:37 +0500

    First commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Transform Player;
+    public int damage = 1;
+    /*
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        hit = hitInfo.GetComponent<Player>();
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+    */
+}
---
+++ Assets/Scripts/ColliderScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColliderScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject finishMenuUI;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Obstacle")
+        {
+            Pause();
+        }
+        if (other.name == "Finish")
+        {
+            Finish();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    void Finish()
+    {
+        finishMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main");
+        Time.timeScale = 1f;
+    }
+}
